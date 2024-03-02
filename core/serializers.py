38e9b4c9e90c597e7fe09335ff5552ee2b13fb7e
Boset <boss-x86@protonmail.com>
2024-03-01 22:54:17 -0600

    alumnos + maestros

---
+++ core/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from core.models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('id','first_name','last_name', 'email')
+
+class AlumnoSerializer(serializers.ModelSerializer):
+    user=UserSerializer(read_only=True)
+    class Meta:
+        model = Alumnos
+        fields = "__all__"
+
+class MaestroSerializer(serializers.ModelSerializer):
+    user=UserSerializer(read_only=True)
+    class Meta:
+        model = Maestros
+        fields = '__all__'
---
+++ core/views/alumnos.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+from django.db.models import *
+from django.db import transaction
+from core.serializers import *
+from core.models import *
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.generics import CreateAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework import permissions
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from django.core import serializers
+from django.utils.html import strip_tags
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
+from datetime import datetime
+from django.conf import settings
+from django.template.loader import render_to_string
+import string
+import random
+import json
+
+class AlumnosView(generics.CreateAPIView):
+    
+    #POST Registrar Alumno
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+
+        user = UserSerializer(data=request.data)
+        if user.is_valid():
+            role = request.data['rol']
+            first_name = request.data['first_name']
+            last_name = request.data['last_name']
+            email = request.data['email']
+            password = request.data['password']
+            
+            #Valida Usuario o Email Registrado
+            existing_user = User.objects.filter(email=email).first()
+
+            if existing_user:
+                return Response({"message":"Username "+email+", is already taken"},400)
+
+            user = User.objects.create( username = email,
+                                        email = email,
+                                        first_name = first_name,
+                                        last_name = last_name,
+                                        is_active = 1)
+
+
+            user.save()
+            user.set_password(password)
+            user.save()
+
+            group, created = Group.objects.get_or_create(name=role)
+            group.user_set.add(user)
+            user.save()
+
+            #Perfil para Usuario
+            alumno = Alumnos.objects.create(user=user,
+                                            matricula= request.data["matricula"],
+                                            curp= request.data["curp"].upper(),
+                                            rfc= request.data["rfc"].upper(),
+                                            fecha_nacimiento= request.data["fecha_nacimiento"],
+                                            edad= request.data["edad"],
+                                            telefono= request.data["telefono"],
+                                            ocupacion= request.data["ocupacion"])
+            alumno.save()
+
+            return Response({"alumno_created_id": alumno.id }, 201)
+
+        return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
