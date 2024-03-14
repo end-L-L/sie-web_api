4de9a33d32d1e6295b69240f8fd1c684b1bf9b72
Boset <boss-x86@protonmail.com>
2024-03-14 16:42:40 -0600

    login + logout

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from .views import alumnos
 from .views import maestros
 from .views import admin
+from .views import auth
 
 urlpatterns = [
+    # Login
+    path('token/', auth.CustomAuthToken.as_view()),
+    # Logout
+    path('logout/', auth.Logout.as_view()),
     # Create Admin
     path('admin/', admin.AdminView.as_view()),
     # Create Alumno
